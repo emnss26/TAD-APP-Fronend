412b6b657589849c56303c40822b8dd9e599debe
Enrique Meneses <emnss26@gmail.com>
2025-05-07 23:48:29 -0600

    reverse flash

--- src/components/general_pages_components/protected.route.jsx
+++ src/components/general_pages_components/protected.route.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { useCookies } from "react-cookie";
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 const ProtectedRoute = () => {
   const [cookies] = useCookies(["access_token"]);
-  return cookies.access_token
-    ? <Outlet />
-    : <Navigate to="/" replace />;
+  const { search } = useLocation();
+  const token = new URLSearchParams(search).get("token");
+
+  // si ya hay cookie *o* viene token en la URL, dejamos pasar
+  if (cookies.access_token || token) {
+    return <Outlet />;
+  }
+
+  return <Navigate to="/not-authorized" replace />;
 };
 
 export default ProtectedRoute;
